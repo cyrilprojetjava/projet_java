2f846db57cd7d40976068328ea60442f4007de2a
Cyril <Cyril@Cyril-PCFIXE.home>
2016-01-29 00:25:27 +0100

    Creation de compte et connexion fonctionnel Reste à vérifier le numero de fiche

---
+++ projet_java/src/projet_java/ThreadTCP.java
@@ -0,0 +1,50 @@
+package projet_java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class ThreadTCP extends Thread{
+
+	private Socket sockService;
+	private GestionProtoAuth gpa;
+	
+	
+public ThreadTCP(Socket sockService, GestionProtoAuth gpa) {
+		super();
+		this.sockService = sockService;
+		this.gpa = gpa;
+	}
+
+
+public void run() {
+		
+		// Instancie un BufferedReader travaillant sur un InputStreamReader lié à  // l’input stream de la socket
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader (new InputStreamReader(sockService.getInputStream()));
+			PrintStream pStream = new PrintStream(sockService.getOutputStream());
+			while(true){
+				// Lit une ligne de caractères depuix le flux, et donc la reçoit du client
+				String requete = reader.readLine();
+				System.out.println(requete);
+				if (requete == null){
+					break;
+				}
+				String reponse = gpa.analyserTraiter(requete);
+				
+				// Instancie un PrintStream travaillant sur l’output stream de la socket PrintStream pStream = new PrintStream(sockService.getOutputStream());
+				// écrit une ligne de caractères sur le flux, et donc l’envoie au client
+				pStream.println(reponse); 
+			}
+			sockService.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
+	
+}
