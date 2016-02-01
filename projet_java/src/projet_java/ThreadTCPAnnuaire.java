d3ee09416caaedc1be0ccc0d83e81a9a3a15c29e
Loic <Loic@LOIC-PC>
2016-02-01 21:32:07 +0100

    Socket entre les serveurs et le client

---
+++ projet_java/src/projet_java/ThreadTCPAnnuaire.java
@@ -0,0 +1,55 @@
+package projet_java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class ThreadTCPAnnuaire extends Thread{
+
+	private Socket sockService;
+	private GestionProtoAnnuaire gpannuaire;
+
+	
+	
+public ThreadTCPAnnuaire(Socket sockService, GestionProtoAnnuaire gpannuaire) {
+		super();
+		this.sockService = sockService;
+		this.gpannuaire = gpannuaire;
+	}
+
+
+
+
+public void run() {
+		
+		// Instancie un BufferedReader travaillant sur un InputStreamReader lieÌ� aÌ€  // lâ€™input stream de la socket
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader (new InputStreamReader(sockService.getInputStream()));
+			PrintStream pStream = new PrintStream(sockService.getOutputStream());
+			while(true){
+				// Lit une ligne de caracteÌ€res depuix le flux, et donc la recÌ§oit du client
+				String requete = reader.readLine();
+				System.out.println(requete);
+				if (requete == null){
+					break;
+				}
+				System.out.println("ccccc");
+				String reponse = gpannuaire.analyserTraiter(requete);
+				//System.out.println(reponse);
+				
+				// Instancie un PrintStream travaillant sur lâ€™output stream de la socket PrintStream pStream = new PrintStream(sockService.getOutputStream());
+				// eÌ�crit une ligne de caracteÌ€res sur le flux, et donc lâ€™envoie au client
+				pStream.println(reponse); 
+			}
+			sockService.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
+	
+}
