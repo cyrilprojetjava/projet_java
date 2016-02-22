40ad4b2175517bf21c56759406db32f234cf2f28
ludovic <ludovic@10.93.2.117>
2016-02-22 14:57:17 +0100

    creation 3 classes demandées par le prof

---
+++ projet_java/src/projet_java/ServCom.java
@@ -0,0 +1,14 @@
+package projet_java;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+
+public class ServCom {
+
+}
---
+++ projet_java/src/projet_java/ThreadCom.java
@@ -0,0 +1,10 @@
+package projet_java;
+
+public class ThreadCom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
